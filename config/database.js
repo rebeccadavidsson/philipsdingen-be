3f470002acbe9bed43f41e150d5d463271fc8cda
Rebecca Davidsson <rebeccadavidsson3@gmail.com>
2022-02-27 09:12:42 +0100

    change ssl

--- config/database.js
+++ config/database.js
@@ -16,6 +16,9 @@ module.exports = ({ env }) => {
             database: config.database,
             username: config.user,
             password: config.password,
+            ssl: {
+              rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+            },
           },
           options: {
             ssl: env.bool('DATABASE_SSL', false),
